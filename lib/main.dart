5365aa6e02c026e300139566dda67f9ba2dab73e
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-03-06 18:10:09 -0600

    Score for questions

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import './result.dart';
 
 void main() => runApp(MyApp());
 
-// ignore: use_key_in_widget_constructors
 class MyApp extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -17,35 +16,36 @@ class _MyAppState extends State<MyApp> {
     {
       'questionText': 'What is your favorite color?',
       'answers': [
-        'Black',
-        'Red',
-        'Green',
-        'White',
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
       ]
     },
     {
       'questionText': 'What is your favorite animal?',
       'answers': [
-        'Dog',
-        'Cat',
-        'Wolf',
-        'Lion',
+        {'text': 'Dog', 'score': 15},
+        {'text': 'Cat', 'score': 3},
+        {'text': 'Wolf', 'score': 11},
+        {'text': 'Lion', 'score': 22},
       ]
     },
     {
       'questionText': 'What is your favorite food?',
       'answers': [
-        'Pizza',
-        'Hamburger',
-        'Pasta Alfredo',
-        'Lasagna',
+        {'text': 'Pizza', 'score': 33},
+        {'text': 'Hamburger', 'score': 12},
+        {'text': 'Pasta Alfredo', 'score': 2},
+        {'text': 'Lasagna', 'score': 11},
       ]
     },
   ];
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
-    if (_questionIndex < _questions.length) {}
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex++;
     });
@@ -64,7 +64,6 @@ class _MyAppState extends State<MyApp> {
                   questions: _questions,
                   questionIndex: _questionIndex,
                 )
-              // ignore: prefer_const_constructors
               : Result()),
     );
   }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -5,7 +5,7 @@ import 'question.dart';
 class Quiz extends StatelessWidget {
   final List<Map<String, Object>> questions;
   final int questionIndex;
-  final VoidCallback answerQuestion;
+  final Function answerQuestion;
 
   Quiz({
     required this.questions,
@@ -20,8 +20,10 @@ class Quiz extends StatelessWidget {
         Question(
           questions[questionIndex]['questionText'].toString(),
         ),
-        ...(questions[questionIndex]['answers'] as List<String>).map((answer) {
-          return Answer(answerQuestion, answer);
+        ...(questions[questionIndex]['answers'] as List<Map<String, Object>>)
+            .map((answer) {
+          return Answer(
+              () => answerQuestion(answer['score']), answer['text'] as String);
         }).toList()
       ],
     );
